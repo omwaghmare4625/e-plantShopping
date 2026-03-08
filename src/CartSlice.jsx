d201a806c29d4c61a31fcae3516c65ddd2d378c8
omwaghmare4625 <omwaghmare6200@gmail.com>
2026-03-08 20:21:26 +0530

    final version

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,13 +7,27 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const plant=action.payload;
+      const exisitingItem=state.items.find((item)=>item.id === plant.id);
+      if(exisitingItem){
+        exisitingItem.quantity +=1;
+      }
+      else{
+        state.items.push({
+          ...plant,quantity:1
+        })
+      }
     },
     removeItem: (state, action) => {
+      const name=action.payload;
+      state.items=state.items.filter((item)=>item.name !== name);
     },
     updateQuantity: (state, action) => {
-
-    
+      const {name,amount} =action.payload;
+      const itemToupdate=state.items.find((item)=> item.name === name);
+      if(itemToupdate){
+        itemToupdate.quantity = amount;
+      }
     },
   },
 });
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,7 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import {useSelector,useDispatch} from 'react-redux';
+import {addItem} from './CartSlice';
 function ProductList({ onHomeClick }) {
+    const dispatch =useDispatch();
+    const cartItems=useSelector((state)=> state.cart.items);
+    const calculateTotalQuantity = () => { 
+    return cartItems 
+        ? cartItems.reduce((total, item) => total + item.quantity, 0) 
+        : 0; 
+    };
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
 
